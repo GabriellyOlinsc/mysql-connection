fe6c8a6a03614d3af926a50da46a4b3d6cec9883
Gabrielly Olinsc <gabrielly-04@outlook.com>
2023-11-25 20:00:19 -0300

    quick update

--- src/main/java/org/example/DataSourceMySQL.java
+++ src/main/java/org/example/DataSourceMySQL.java
@@ -8,9 +8,9 @@ public class DataSourceMySQL {
     public Connection conectaBD() throws SQLException ,ClassNotFoundException {
         String url = "jdbc:mysql://209.209.40.91:19860/dbConnection_M3?useSSL=false&serverTimezone=UTC";
         String user = "admin";
-        String password = "w77LFoqf";
+        String passwd = "w77LFoqf";
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection con = DriverManager.getConnection(url, user, password);
+        Connection con = DriverManager.getConnection(url, user, passwd);
         return con;
     }
 }
